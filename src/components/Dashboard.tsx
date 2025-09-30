192ade2a66ec3eb144779d6da157e8e2a5d0092b
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-30 12:01:20 +0200

    fetch orders

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
-import type { Employee } from "../types";
+import type { Order, Employee } from "../types";
 
 const backendUrl = "http://localhost:3399";
 const token = "abcde12345";
 
 export const Dashboard = () => {
 	const [employees, setEmployees] = useState<Employee[]>([]);
+	const [orders, setOrders] = useState<Order[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -19,9 +20,22 @@ export const Dashboard = () => {
 		})();
 	}, [])
 
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(`${backendUrl}/northwind/orders`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const _orders = await response.json();
+			setOrders(_orders);
+		})();
+	}, [])
+
 	return (
 		<div className="mt-3 bg-slate-200 p-3 rounded">
 			<p>Employees: {employees.length}</p>
+			<p>Orders: {orders.length}</p>
 		</div>
 	)
 }
