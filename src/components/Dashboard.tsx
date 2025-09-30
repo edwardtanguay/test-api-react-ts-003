5f9f28c82d52027ac10db3c98a5a45941bd4d38b
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-30 12:07:28 +0200

    Make dropdown

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import type { Order, Employee } from "../types";
+import { type Order, type Employee, type Customer } from "../types";
 
 const backendUrl = "http://localhost:3399";
 const token = "abcde12345";
@@ -7,6 +7,7 @@ const token = "abcde12345";
 export const Dashboard = () => {
 	const [employees, setEmployees] = useState<Employee[]>([]);
 	const [orders, setOrders] = useState<Order[]>([]);
+	const [customers, setCustomers] = useState<Customer[]>([]);
 
 	useEffect(() => {
 		(async () => {
@@ -32,10 +33,28 @@ export const Dashboard = () => {
 		})();
 	}, [])
 
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(`${backendUrl}/northwind/customers`, {
+				headers: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+			const _customers = await response.json();
+			setCustomers(_customers);
+		})();
+	}, [])
+
 	return (
-		<div className="mt-3 bg-slate-200 p-3 rounded">
-			<p>Employees: {employees.length}</p>
-			<p>Orders: {orders.length}</p>
+		<div className="mt-3 bg-slate-300 p-3 rounded">
+
+			<select className="p-1 rounded">
+				{employees.map(emp => {
+					return (
+						<option key={emp.employeeID} value={emp.employeeID}>{emp.firstName} {emp.lastName}</option>
+					)
+				})}
+			</select>
 		</div>
 	)
 }
